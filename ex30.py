d43623b087a6798b6bc0319b21b2e65490b15512
Brandon Walsh <bmw9t@virginia.edu>
2013-08-14 10:56:46 -0400

    new exercises.

--- ex30.py
+++ ex30.py
@@ -14,3 +14,10 @@ if buses > cars:
 	print "That's too many buses."
 elif buses < cars:
 	print "Maybe we could take the buses."
+else:
+	print "We still can't decide."
+
+if people > buses:
+	print "Alright, let's just take the buses."
+else:
+	print "Fine, let's stay home then."
